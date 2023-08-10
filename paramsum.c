fa7fb7e785c8cd61b4f5bea58aa4fb2500e9d9d6
laawis <115143274+laawis@users.noreply.github.com>
2023-08-10 14:46:50 +0200

    Add files via upload

---
+++ paramsum.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_putnbr(int i)
+{
+    if (i > 9)
+        ft_putnbr(i / 10);
+    ft_putchar((i % 10) + '0');
+}
+
+int main(int argc, char **argv)
+{
+    int i;
+    (void) (argv);
+
+    if (argc > 1)
+    {
+        i = argc - 1;
+        ft_putnbr(i);
+    }
+    if (argc == 1)
+        ft_putchar('0');
+    ft_putchar('\n');
+    return (0);
+}
