307de0bab39af057a40ac7694e089f78e2b6a77a
laawis <115143274+laawis@users.noreply.github.com>
2023-08-11 09:31:02 +0200

    Update rostring.c

--- rostring.c
+++ rostring.c
@@ -1,5 +1,7 @@
 #include <unistd.h>
 
+// ROSTRING FIRST (NOT PIMPED)
+
 int    main(int argc, char **argv)
 {
     int    start;
@@ -45,36 +47,79 @@ int    main(int argc, char **argv)
        }
        write(1, "\n", 1);
        return (0);
-     /*
-    if (argc > 1 && argv[1][0])
+}
+// ROSTRING PIMPED
+// WARNING LA VARIABLE int return_code; n'est utilisée que pour 
+// le nouveau compilateur gcc qui demande d'utiliser le retour de write sans quoi ça ne compile pas.
+int    skip_ispace(char *str, int i)
+{
+    while (str[i] == ' ' || str[i] == '\t')
+        i++;
+    return (i);
+}
+
+int    skip_word(char *str, int i)
+{
+    while (str[i] != '\0' && str[i] != ' ' && str[i] != '\t')
+        i++;
+    return (i);
+}
+
+void    print_first_word(char *str, int start, int end)
+{
+    int    return_code;
+
+    return_code = 0;
+    while (start < end)
     {
-        i = 0;
-        while (argv[1][i] == ' ' || argv[1][i] == '\t')
-            i++;
-        start = i;
-        while (argv[1][i] != '\0' && argv[1][i] != ' ' && argv[1][i] != '\t')
-            i++;
-        end = i;
-        while (argv[1][i] == ' ' || argv[1][i] == '\t')
-            i++;
-        while(argv[1][i]) 
-        {
-            while ((argv[1][i] == ' ' && argv[1][i + 1] == ' ') || (argv[1][i] == '\t' && argv[1][i + 1] == '\t'))
-                i++; 
-            if (argv[1][i] == ' ' || argv[1][i] == '\t')
-                flag = 1; 
-            write(1, &argv[1][i], 1);
-            i++;
-        }
-        if (flag)
-            write(1, " ", 1);
-        while (start < end)
-        {
-            write(1, &argv[1][start], 1);
-            start++;
-        }
+        return_code = write(1, &str[start], 1);
+        (void) return_code;
+        start++;
     }
-    write(1, "\n", 1);
-    return(0);
-    */
+}
+
+int    main(int argc, char **argv)
+{
+    int    return_code;
+    int    start;
+    int    end;
+    int    i;
+    int    flag;
+
+    i = 0;
+    flag = 0;
+
+    if (argc > 1 && argv[1][i] != '\0')
+    {
+        i = skip_ispace(argv[1], i);
+        start = i;
+        i = skip_word(argv[1], i);
+        end = i;
+        i = skip_ispace(argv[1], i);
+        while(argv[1][i] != '\0')
+        {
+            while (argv[1][i] != '\0' && argv[1][i] != ' ' && argv[1][i] != '\t')
+            {
+                return_code = write(1, &argv[1][i], 1);
+                (void) return_code;
+                flag = 1;
+                i++;
+            }
+            i = skip_ispace (argv[1], i);
+            if (argv[1][i] != '\0' && (argv[1][i - 1] == ' '|| argv[1][i - 1] == '\t'))
+            {
+                return_code = write(1, " ", 1);
+                (void) return_code;
+            }
+        }
+        if (flag == 1)
+        {
+            return_code = write(1, " ", 1);
+            (void) return_code;
+        }
+        print_first_word(argv[1], start, end);
+    }
+    return_code = write(1, "\n", 1);
+    (void) return_code;
+    return (0);
 }
