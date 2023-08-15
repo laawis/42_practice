3d5d9e339edf8e5e4e0196c9ae5cbf734a6a47ab
laawis <115143274+laawis@users.noreply.github.com>
2023-08-15 19:22:30 +0200

    Create ft_strcmp.c

---
+++ ft_strcmp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int ft_strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	
+	while(s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+	{	
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}	
+
+
+Assignment name	: ft_strcspn
+Expected files	: ft_strcspn.c
+Allowed functions: None
+---------------------------------------------------------------
+
+Reproduce exactly the behavior of the function strcspn
+(man strcspn).
+
+The function should be prototyped as follows:
+
+size_t	ft_strcspn(const char *s, const char *reject);
