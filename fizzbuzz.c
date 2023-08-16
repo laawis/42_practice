6f49f5f988ccbe46a7526b6b007dc0eb1412cd3d
laawis <115143274+laawis@users.noreply.github.com>
2023-08-16 11:24:31 +0200

    Update fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,5 +1,42 @@
 #include <unistd.h>
+// Août 2023
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
 
+void    ft_putnbr(int nb)
+{
+    if (nb <= 9)
+        nb = nb;
+    if (nb > 9)
+        ft_putnbr(nb / 10);
+    ft_putchar(nb % 10 + '0');
+}
+
+int main(void)
+{
+    int i = 1;
+
+    while (i <= 100)
+    {
+        if (i % 3 == 0 && i % 5 != 0)
+            write(1, "fizz", 4);
+        else if (i % 3 != 0 && i % 5 == 0)
+            write(1, "buzz", 4);
+        else if (i % 3 == 0 && i % 5 == 0)
+            write(1, "fizzbuzz", 8);
+        else
+            ft_putnbr(i);
+        write(1, "\n", 1);
+        i++;
+    }
+    return (0);
+}
+
+
+
+// Septemnbre 2022
 void	ft_putchar(char c)
 {
 	write(1, &c, 1);
