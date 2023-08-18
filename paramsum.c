c53e23759737391037ea7988daaea8c0da69e2ee
laawis <115143274+laawis@users.noreply.github.com>
2023-08-18 12:54:04 +0200

    Update paramsum.c

--- paramsum.c
+++ paramsum.c
@@ -1,5 +1,40 @@
 #include <unistd.h>
+// PARAMSUM 2
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
 
+void    ft_putnbr(int nb)
+{
+    if (nb > 9)
+        ft_putnbr(nb / 10);
+    ft_putchar((nb % 10) + '0');
+}
+
+void    paramsum(int argc)
+{
+    int i;
+
+    if (argc > 1)
+    {
+        i = argc - 1;
+        ft_putnbr(i);
+    }
+    if (argc == 1)
+        write(1, "0", 1);
+    write(1, "\n", 1);
+}
+
+int main(int argc, char **argv)
+{
+    (void) argv;
+    paramsum(argc);
+    return (0);
+}
+
+
+// PARAMSUM 1
 void ft_putchar(char c)
 {
     write(1, &c, 1);
