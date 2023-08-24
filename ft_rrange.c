67c5711e939f34a4d4c9fe14bcd3f0649330a264
laawis <115143274+laawis@users.noreply.github.com>
2023-08-24 15:11:54 +0200

    Update ft_rrange.c

--- ft_rrange.c
+++ ft_rrange.c
@@ -7,14 +7,14 @@ int *ft_rrange(int start, int end)
     int n;
 
     i = 0;
-    n = end - start + 1;
+    n = end - start;
 
     if (start > end)
         return (ft_rrange(end, start));
-    range = malloc(sizeof(int) * n);
+    range = malloc(sizeof(int) * (n + 1));
     if (range)
     {
-        while (i < n)
+        while (i <= n)
         {
             range[i] = end;
             end--;
@@ -23,4 +23,24 @@ int *ft_rrange(int start, int end)
     }
     return (range);
 }
-        
+/*       
+Assignment name  : ft_rrange
+Expected files   : ft_rrange.c
+Allowed functions: malloc
+--------------------------------------------------------------------------------
+
+Write the following function:
+
+int     *ft_rrange(int start, int end);
+
+It must allocate (with malloc()) an array of integers, fill it with consecutive
+values that begin at end and end at start (Including start and end !), then
+return a pointer to the first value of the array.
+
+Examples:
+
+- With (1, 3) you will return an array containing 3, 2 and 1
+- With (-1, 2) you will return an array containing 2, 1, 0 and -1.
+- With (0, 0) you will return an array containing 0.
+- With (0, -3) you will return an array containing -3, -2, -1 and 0.
+*/
