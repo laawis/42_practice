78b615bc1e0fb467d36c4da9cfdf66a43298e914
laawis <115143274+laawis@users.noreply.github.com>
2023-08-30 23:27:13 +0200

    Update str_capitalizer.c

--- str_capitalizer.c
+++ str_capitalizer.c
@@ -1,5 +1,42 @@
 #include <unistd.h>
 
+VERSION MOI
+void    str_capitalizer(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 'A' && str[i] <= 'Z')
+            str[i] += 32;
+        if ((str[i - 1] == ' ' || str[i - 1] == '\t') && (str[i] >= 'a' && str[i] <= 'z'))
+            str[i] -= 32;
+        if (str[0] >= 'a' && str[0] <= 'z')
+            str[0] -= 32;
+        write(1, &str[i], 1);
+        i++;
+    }
+    write(1, "\n", 1);
+}
+
+int main(int argc, char **argv)
+{
+    int i = 1;
+
+    if (argc > 1)
+    {
+        while (i <= argc)
+        {
+            str_capitalizer(argv[i]);
+            i++;
+        }
+    }
+    write(1, "\n", 1);
+    return (0);
+}
+
+
+VERSION 2
 void    str_capitalizer(char *str)
 {
     int i;
