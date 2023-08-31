d9b3078101d89d378254720c4a9c9e7541e28ee1
laawis <115143274+laawis@users.noreply.github.com>
2023-08-31 10:18:12 +0200

    Update ft_strcmp.c

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -10,16 +10,35 @@ int ft_strcmp(char *s1, char *s2)
 	}
 	return (s1[i] - s2[i]);
 }	
+/*
+MAN PAGE
+NAME
+     strcmp, strncmp -- compare strings
 
+LIBRARY
+     Standard C Library (libc, -lc)
 
-Assignment name	: ft_strcspn
-Expected files	: ft_strcspn.c
-Allowed functions: None
----------------------------------------------------------------
+SYNOPSIS
+     #include <string.h>
 
-Reproduce exactly the behavior of the function strcspn
-(man strcspn).
+     int
+     strcmp(const char *s1, const char *s2);
 
-The function should be prototyped as follows:
+     int
+     strncmp(const char *s1, const char *s2, size_t n);
 
-size_t	ft_strcspn(const char *s, const char *reject);
+DESCRIPTION
+     The strcmp() and strncmp() functions lexicographically compare the null-
+     terminated strings s1 and s2.
+
+     The strncmp() function compares not more than n characters.  Because
+     strncmp() is designed for comparing strings rather than binary data,
+     characters that appear after a `\0' character are not compared.
+
+RETURN VALUES
+     The strcmp() and strncmp() functions return an integer greater than,
+     equal to, or less than 0, according as the string s1 is greater than,
+     equal to, or less than the string s2.  The comparison is done using
+     unsigned characters, so that `\200' is greater than `\0'.
+*/
+
